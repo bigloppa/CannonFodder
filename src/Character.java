4f64a695d40204e82b1c5c1f2f85278ca242f81b
bigloppa <egedenizyasar40@hotmail.com>
2022-04-30 23:56:44 +0300

    d

---
+++ src/Character.java
@@ -0,0 +1,12 @@
+
+public abstract class Character {
+    String name;
+    int hp;
+    int vitality;
+    int strength;
+    int intelligence;
+    Weapon weapon;
+    Clothing clothing;
+
+
+}
---
+++ src/Wand.java
@@ -0,0 +1,7 @@
+public class Wand extends Weapon {
+
+    public void heal(){
+
+    }
+
+}
